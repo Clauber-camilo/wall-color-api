93d6e990914ccbf716d131118458f19a4f1c1a1d
Clauber  Camilo <Clauber@Claubers-MacBook-Pro.local>
2017-08-30 13:07:32 -0300

    Update structure and dependencies

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-let express = require('express')
-  , app = express()
-  , db = require('./db')
+// let express = require('express')
+//   , app = express()
+//   , db = require('./db')
 
-let UserController = require('./user/userController')  
+// let UserController = require('./user/userController')  
 
-let PhotoController = require('./photos/photoController')
+// let PhotoController = require('./photos/photoController')
 
-app.use('/photos', PhotoController)
+// app.use('/photos', PhotoController)
 
-app.use('/users', UserController)
+// app.use('/users', UserController)
+
+// module.exports = app
+
+const app = require('koa')
+    , logger = require('koa-logger')
+
+
+app.use(logger)
+
+
+
 
 module.exports = app
