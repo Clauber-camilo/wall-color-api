8603456f3ea3d5154a2e19bb9f8fb0779a0601b9
Clauber Camilo <clauber_camilo@live.com>
2017-09-03 19:23:42 -0300

    First hit

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ const Koa = require('koa')
     , router = require('koa-router')()
     , koaBody = require('koa-body')
     , axios = require('axios')
-    , getColors = require('get-image-colors')
-    , app = new Koa()
+
+
+const getColors = require('get-image-colors')
+    , streams = ('memory-streams')
+    , fs = require('fs')
+
+const app = new Koa()
 
 
 const mock = require('./src/lib/mockup')
 
 app.use(logger())
 
-function log () {
-    console.log(getColors)
+function log (log) {
+    console.log(log)
 }
-
-log()
 
 
 // var http = require('http');
@@ -32,22 +35,63 @@ log()
 //     });
 //   });
 // }
-
+const util = require('util')
 router.get('/', async (ctx) => {
 
     // const photoList = await axios.get('https://unsplash.it/list')
 
-    let pallete
+    let pallete = []
+    let image
+    let colors
 
-    let photoList = await mock.forEach((photo) => {
-        pallete = {
-            id: photo.id,
-            image: photo.urls.regular,
-            colors: getColors(photo.urls.regular).then(colors => colors.map(c => c))
-        }
+    // for (let item of mock) {
 
-        return pallete
-    });
+        
+
+    // }
+
+    // image = await axios.get(mock[0].urls.regular)
+
+    // await axios({
+    //     method:'get',
+    //     url: mock[0].urls.regular,
+    //     responseType:'stream'
+    // })
+    // .then((response) => {
+    //     log(response.data)
+    //     image = response.data.pipe(fs.createWriteStream('./test.jpg'))
+    // });
+    
+    // fs.mkdirpSync('./images')
+    // fs.writeFileSync('./images/image.jpg', image)
+    // log(util.inspect(image.data, {depth: null}))
+
+    // fs.writeFile('./image.jpg', image.data, function(err){
+    //     if (err) throw err
+    //     console.log('File saved.')
+    // })
+
+
+    await getColors('./test.jpg').then(c => {
+        colors = c.map(color => color.hex())
+    })
+            
+    log(colors)
+
+    pallete.push({
+        id: mock[0].id,
+        cores: colors
+    })
+
+    // mock.forEach((photo) => {
+    //     pallete = {
+    //         id: photo.id,
+    //         image: photo.urls.regular,
+    //         colors: getColors(photo.urls.regular).then(colors => colors.map(c => c))
+    //     }
+
+    //     return pallete
+    // });
 
     ctx.body = pallete // list of images
 })
