6433c3e24aaf3d069817c708d6513ed2d461b167
Clauber  Camilo <Clauber@Claubers-MacBook-Pro.local>
2017-09-05 08:44:25 -0300

    First Success

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const Koa = require('koa')
 
 
 const getColors = require('get-image-colors')
-    , streams = ('memory-streams')
-    , fs = require('fs')
+    , MemoryFileSystem = require("memory-fs")
+    , fs = new MemoryFileSystem()
 
 const app = new Koa()
 
@@ -22,78 +22,47 @@ function log (log) {
     console.log(log)
 }
 
-
-// var http = require('http');
-// var fs = require('fs');
-
-// var download = function(url, dest, cb) {
-//   var file = fs.createWriteStream(dest);
-//   var request = http.get(url, function(response) {
-//     response.pipe(file);
-//     file.on('finish', function() {
-//       file.close(cb);
-//     });
-//   });
-// }
 const util = require('util')
 router.get('/', async (ctx) => {
 
     // const photoList = await axios.get('https://unsplash.it/list')
 
-    let pallete = []
-    let image
+    let list = []
     let colors
 
-    // for (let item of mock) {
+    fs.mkdirpSync("/public/images/")
 
-        
+    for (let item of mock) {
 
-    // }
-
-    // image = await axios.get(mock[0].urls.regular)
-
-    // await axios({
-    //     method:'get',
-    //     url: mock[0].urls.regular,
-    //     responseType:'stream'
-    // })
-    // .then((response) => {
-    //     log(response.data)
-    //     image = response.data.pipe(fs.createWriteStream('./test.jpg'))
-    // });
+        await axios({
+            method:'get',
+            url: item.urls.regular,
+            responseType:'arraybuffer'
+        })
+        .then((response) => {            
+            fs.writeFileSync('/public/images/test.jpg', response.data)
+        })
+        .catch(error => {
+             log(error)
+        })
     
-    // fs.mkdirpSync('./images')
-    // fs.writeFileSync('./images/image.jpg', image)
-    // log(util.inspect(image.data, {depth: null}))
+    
+        await getColors(fs.readFileSync('/public/images/test.jpg'), 'image/jpg').then(c => {
+            colors = c.map(color => color.hex())
+        })
 
-    // fs.writeFile('./image.jpg', image.data, function(err){
-    //     if (err) throw err
-    //     console.log('File saved.')
-    // })
+        fs.unlink('/public/images/test.jpg', err => err)
 
+        list.push({
+            id: item.id,
+            url: item.urls.regular,
+            cores: colors
+        }) 
 
-    await getColors('./test.jpg').then(c => {
-        colors = c.map(color => color.hex())
-    })
-            
-    log(colors)
+    }
+    fs.rmdirSync('/public/images/')
 
-    pallete.push({
-        id: mock[0].id,
-        cores: colors
-    })
-
-    // mock.forEach((photo) => {
-    //     pallete = {
-    //         id: photo.id,
-    //         image: photo.urls.regular,
-    //         colors: getColors(photo.urls.regular).then(colors => colors.map(c => c))
-    //     }
-
-    //     return pallete
-    // });
-
-    ctx.body = pallete // list of images
+    ctx.body = list // list of images
 })
 
 app 
--- server.js
+++ server.js
@@ -3,5 +3,5 @@ let app = require('./app')
 let port = process.env.PORT || 3000 
 
 let server = app.listen(port, () => {
-    console.log(`The magic will happen on port ${port}`)
+    console.log(`🚀  The magic will happen on port ${port}`)
 })
