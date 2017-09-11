b870a8d31e232df5d2e301ddfda1cbd458da4914
Clauber  Camilo <Clauber@Claubers-MacBook-Pro.local>
2017-09-11 08:45:29 -0300

    Implement params in the save-images route

--- src/photos/PhotoController.js
+++ src/photos/PhotoController.js
@@ -29,62 +29,67 @@ class PhotoController {
 
     async saveAll (ctx) {
         console.log('Aehooooo')
-
-        let list = { request: {}}
-        let colorArray
-        let save = []
-
-
-        fs.mkdirpSync("/public/images/")
-
-        const unsplashResponse = await axios({
-            method: 'GET',
-            url: `${process.env.UNSPLASH_URL}/photos?per_page=100`,
-            headers: {
-                'Authorization' : `Client-ID ${process.env.UNSPLASH_CLIENT_ID}`
-            }
-        })
-
-        console.log(unsplashResponse)
-
-        for (let item in unsplashResponse) {
-
-            await axios({
-                method:'get',
-                url: item.urls.regular,
-                responseType:'arraybuffer'
-            })
-            .then((response) => {            
-                fs.writeFileSync('/public/images/test.jpg', response.data)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-        
-            await getColors(fs.readFileSync('/public/images/test.jpg'), 'image/jpg').then(c => {
-                colorArray = c.map(color => color.hex())
-            })
-
-            fs.unlink('/public/images/test.jpg', err => err)        
-
-            list.request.body = {
-                id: item.id,
-                url: { 
-                    full: item.urls.full,
-                    regular: item.urls.regular 
+        try {
+            let list = { request: {}}
+            let colorArray
+            let save = []
+    
+    
+            fs.mkdirpSync("/public/images/")
+    
+            const unsplashResponse = await axios({
+                method: 'GET',
+                url: `${process.env.UNSPLASH_URL}/photos`,
+                headers: {
+                    'Authorization' : `Client-ID ${process.env.UNSPLASH_CLIENT_ID}`
                 },
-                colors: colorArray
-            }
-
-            // console.log(list)
-            const photo = await new Photo(list.request.body).save()
-            save.push(photo) 
+                params: {
+                    page: ctx.request.query ? ctx.request.query.page : null,
+                    per_page: ctx.request.query ? ctx.request.query.per_page : null
+                }
+            })
+    
+            for (let item of unsplashResponse.data) {
+        
+                await axios({
+                    method:'get',
+                    url: item.urls.regular,
+                    responseType:'arraybuffer'
+                })
+                .then((response) => {            
+                    fs.writeFileSync('/public/images/test.jpg', response.data)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
             
-        }
-
-        fs.rmdirSync('/public/images/')
-
-        ctx.body = save // list of images
+                await getColors(fs.readFileSync('/public/images/test.jpg'), 'image/jpg').then(c => {
+                    colorArray = c.map(color => color.hex())
+                })
+    
+                fs.unlink('/public/images/test.jpg', err => err)        
+    
+                list.request.body = {
+                    id: item.id,
+                    url: { 
+                        full: item.urls.full,
+                        regular: item.urls.regular 
+                    },
+                    colors: colorArray
+                }
+    
+                // console.log(list)
+                const photo = await new Photo(list.request.body).save()
+                save.push(photo) 
+                
+            }
+    
+            fs.rmdirSync('/public/images/')
+    
+            ctx.body = save // list of images
+        } catch(e) {
+            console.log(e)
+        }    
     }
 }
 
--- src/photos/route.js
+++ src/photos/route.js
@@ -17,6 +17,8 @@ router.get('/:id', PhotoController.findById)
 
 // GET /api/photos/save-images 
 // This will save a list of images of the unplash in the mongo database
+// @param : page (default: 1)
+// @param : per_page (default : 10)
 router.post('/save-images', PhotoController.saveAll)
 
 module.exports = router
